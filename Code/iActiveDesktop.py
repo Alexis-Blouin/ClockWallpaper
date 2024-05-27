cec522a7eb10b16a389525558e38d0675303a971
AlexisBlouin <blouin.alexis@icloud.com>
2024-05-27 14:14:39 -0400

    SystemParametersInfo won't work at all, it can only change all the monitors at the same time

--- Code/iActiveDesktop.py
+++ Code/iActiveDesktop.py
@@ -1,9 +1,10 @@
+import os
 import comtypes.client
 import ctypes
 from ctypes import wintypes
 
 # Generate the SHDocVw type library
-comtypes.client.GetModule('shdocvw.dll')
+comtypes.client.GetModule("shdocvw.dll")
 
 # Import the generated SHDocVw module
 import comtypes.gen.SHDocVw as SHDocVw
@@ -12,43 +13,119 @@ import comtypes.gen.SHDocVw as SHDocVw
 CLSID_ActiveDesktop = comtypes.GUID("{75048700-EF1F-11D0-9888-006097DEACF9}")
 IID_IActiveDesktop = comtypes.GUID("{F490EB00-1240-11D1-9888-006097DEACF9}")
 
+
 # Define the IActiveDesktop interface
 class IActiveDesktop(comtypes.IUnknown):
     _iid_ = IID_IActiveDesktop
     _methods_ = [
         comtypes.STDMETHOD(ctypes.HRESULT, "ApplyChanges", [ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "GetWallpaper", [wintypes.LPWSTR, ctypes.c_uint, ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "SetWallpaper", [wintypes.LPCWSTR, ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "GetWallpaperOptions", [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "SetWallpaperOptions", [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "GetPattern", [wintypes.LPWSTR, ctypes.c_uint, ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "SetPattern", [wintypes.LPCWSTR, ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "GetDesktopItemOptions", [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "SetDesktopItemOptions", [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "AddDesktopItem", [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "AddDesktopItemWithUI", [wintypes.HWND, ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "ModifyDesktopItem", [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "RemoveDesktopItem", [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "GetDesktopItemByID", [ctypes.c_ulong, ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "GetDesktopItemWithIDList", [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong, ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "GetDesktopItem", [ctypes.c_ulong, ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "GetDesktopItemList", [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "GetDesktopItemState", [ctypes.c_ulong, ctypes.c_ulong, ctypes.POINTER(ctypes.c_ulong)]),
-        comtypes.STDMETHOD(ctypes.HRESULT, "SetDesktopItemState", [ctypes.c_ulong, ctypes.c_ulong, ctypes.c_ulong]),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "GetWallpaper",
+            [wintypes.LPWSTR, ctypes.c_uint, ctypes.c_ulong],
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT, "SetWallpaper", [wintypes.LPCWSTR, ctypes.c_ulong]
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "GetWallpaperOptions",
+            [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong],
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "SetWallpaperOptions",
+            [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong],
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "GetPattern",
+            [wintypes.LPWSTR, ctypes.c_uint, ctypes.c_ulong],
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT, "SetPattern", [wintypes.LPCWSTR, ctypes.c_ulong]
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "GetDesktopItemOptions",
+            [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong],
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "SetDesktopItemOptions",
+            [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong],
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "AddDesktopItem",
+            [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong],
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "AddDesktopItemWithUI",
+            [wintypes.HWND, ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong],
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "ModifyDesktopItem",
+            [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong],
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "RemoveDesktopItem",
+            [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong],
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "GetDesktopItemByID",
+            [ctypes.c_ulong, ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong],
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "GetDesktopItemWithIDList",
+            [
+                ctypes.POINTER(wintypes.LPVOID),
+                ctypes.c_ulong,
+                ctypes.POINTER(wintypes.LPVOID),
+                ctypes.c_ulong,
+            ],
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "GetDesktopItem",
+            [ctypes.c_ulong, ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong],
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "GetDesktopItemList",
+            [ctypes.POINTER(wintypes.LPVOID), ctypes.c_ulong],
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "GetDesktopItemState",
+            [ctypes.c_ulong, ctypes.c_ulong, ctypes.POINTER(ctypes.c_ulong)],
+        ),
+        comtypes.STDMETHOD(
+            ctypes.HRESULT,
+            "SetDesktopItemState",
+            [ctypes.c_ulong, ctypes.c_ulong, ctypes.c_ulong],
+        ),
     ]
+
 
 def get_active_desktop():
     # Initialize COM
     comtypes.CoInitialize()
 
     # Get the IShellDispatch2 interface from Shell.Application
-    #shell = comtypes.client.CreateObject("Shell.Application")
-    #shell_dispatch = shell.QueryInterface(SHDocVw.IShellDispatch)
+    # shell = comtypes.client.CreateObject("Shell.Application")
+    # shell_dispatch = shell.QueryInterface(SHDocVw.IShellDispatch)
 
     # Get the IActiveDesktop interface
-    iad = comtypes.client.CreateObject("CLSID_ActiveDesktop, interface=IActiveDesktop")
+    iad = comtypes.client.CreateObject(CLSID_ActiveDesktop, interface=IActiveDesktop)
 
     return iad
+
 
 def set_wallpaper_per_monitor(wallpaper_paths):
     print("Avant get desktop")
@@ -63,9 +140,12 @@ def set_wallpaper_per_monitor(wallpaper_paths):
     comtypes.CoUninitialize()
     print("fin")
 
+
 if __name__ == "__main__":
     wallpaper_paths = [
         # Replace with your actual image paths
-        r"C:\Users\Alexis\Documents\GitHub\WallpaperSwitch\Aoi Ogata.png"
+        r"C:\Users\Alexis\Documents\GitHub\WallpaperSwitch\Images\Aoi Ogata.png"
     ]
     set_wallpaper_per_monitor(wallpaper_paths)
+
+    print(os.getlogin())
--- Code/switchWallpaper.py
+++ Code/switchWallpaper.py
@@ -4,56 +4,49 @@ import win32api, win32con
 from PIL import Image, ImageDraw, ImageFont
 import comtypes.client
 
+# Generate the SHDocVw type library
+comtypes.client.GetModule("shdocvw.dll")
 
-def saveWallpaper(user, imageName, monitorId):
+# Import the generated SHDocVw module
+import comtypes.gen.SHDocVw as SHDocVw
+
+user = os.getlogin()
+
+
+def saveWallpaper(imageName, ext, monitorId):
     winWallpaperPath = (
         r"C:\Users\\" + user + r"\AppData\Roaming\Microsoft\Windows\Themes"
     )
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    relative_path = os.path.join("..", "Images", imageName + ".jpg")
+    relative_path = os.path.join("..", "Images", imageName + ext)
     image = Image.open(os.path.join(current_dir, relative_path))
-    image.save(os.path.join(winWallpaperPath, f"Transcoded_00{monitorId}.jpg"))
+    image.save(os.path.join(winWallpaperPath, f"Transcoded_00{monitorId}" + ext))
+    image.save(os.path.join(winWallpaperPath, f"TranscodedWallpaper" + ext))
 
     if os.path.exists(os.path.join(winWallpaperPath, f"Transcoded_00{monitorId}")):
         os.remove(os.path.join(winWallpaperPath, f"Transcoded_00{monitorId}"))
+    if os.path.exists(os.path.join(winWallpaperPath, f"TranscodedWallpaper")):
+        os.remove(os.path.join(winWallpaperPath, f"TranscodedWallpaper"))
 
-    # Renames the image to remove the extension
+    # # Renames the image to remove the extension
     os.rename(
-        os.path.join(winWallpaperPath, "Transcoded_00" + monitorId + ".jpg"),
+        os.path.join(winWallpaperPath, "Transcoded_00" + monitorId + ext),
         os.path.join(winWallpaperPath, "Transcoded_00" + monitorId),
     )
-
-
-def changeWallpaper():
-    # Get the absolute path of the current directory
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-
-    # Construct the relative path from the current directory
-    relative_path = os.path.join("..", "Images", "MiniNez.png")
-
-    # Get the absolute path using the constructed relative path
-    absolute_path = os.path.normpath(os.path.join(current_dir, relative_path))
-
-    # Set the desktop background using the absolute path
-    # ctypes.windll.user32.SystemParametersInfoW(0, 0, None, 0x0001 | 0x0002)
-
-    SPI_SETDESKWALLPAPER = 0x0014
-    SPIF_UPDATEINIFILE = 0x0001
-    SPIF_SENDCHANGE = 0x0002
-
-    # Refresh the current wallpaper
-    ctypes.windll.user32.SystemParametersInfoW(
-        SPI_SETDESKWALLPAPER,
-        0,
-        absolute_path,
-        SPIF_UPDATEINIFILE | SPIF_SENDCHANGE,
+    os.rename(
+        os.path.join(winWallpaperPath, "TranscodedWallpaper" + ext),
+        os.path.join(winWallpaperPath, "TranscodedWallpaper"),
     )
 
 
 def updateWallpaper():
     # Replace "username" with the actual username
-    wallpaper_path = r"C:\Users\username\AppData\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper"
+    wallpaper_path = (
+        r"C:\Users\\"
+        + user
+        + r"\AppData\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper"
+    )
     # Simulate a wallpaper change by sending a message
     # win32api.SendMessage(win32con.HWND_BROADCAST, win32con.WM_DEACTIVATE, 0, 0)
     # win32api.SendMessage(win32con.HWND_BROADCAST, win32con.WM_ACTIVATEAPP, 0, 0)
@@ -66,23 +59,24 @@ def set_wallpaper_per_monitor():
     # Initialize COM
     comtypes.CoInitialize()
 
+    CLSID_ActiveDesktop = comtypes.GUID("{75048700-EF1F-11D0-9888-006097DEACF9}")
     # Create an instance of IActiveDesktop
-    iad = comtypes.client.CreateObject("ActiveDesktop")
+    iad = comtypes.client.CreateObject(CLSID_ActiveDesktop)
 
     # Define the paths to your wallpapers for each monitor
     wallpaper_paths = [
         # Replace with your actual image paths
-        r"C:\Users\Alexis\Documents\GitHub\WallpaperSwitch\MiniNez.png",
-        r"C:\Users\Alexis\Documents\GitHub\WallpaperSwitch\Aoi Ogata.jpg",
-        r"C:\Users\Alexis\Documents\GitHub\WallpaperSwitch\LucySexy.jpg",
+        r"C:\Users\Alexis\Documents\GitHub\WallpaperSwitch\Images\MiniNez.png",
+        r"C:\Users\Alexis\Documents\GitHub\WallpaperSwitch\Images\Aoi Ogata.jpg",
+        r"C:\Users\Alexis\Documents\GitHub\WallpaperSwitch\Images\LucySexy.jpg",
     ]
     wallpaper_path = [
         # Replace with your actual image paths
-        r"C:\Users\Alexis\Documents\GitHub\WallpaperSwitch\MiniNez.png"
+        r"C:\Users\Alexis\Documents\GitHub\WallpaperSwitch\Images\MiniNez.png"
     ]
 
     # Get number of monitors (optional)
-    monitors = iad.GetMonitorDEsktopCount()
+    monitors = iad.GetMonitorDesktopCount()
 
     # Set wallpaper for each monitor (using enumerate or explicit indices)
     # Choose the method that suits your needs
@@ -99,6 +93,5 @@ def set_wallpaper_per_monitor():
     comtypes.CoUninitialize()
 
 
-# saveWallpaper("Alexis", "Aoi Ogata", "1")
-# changeWallpaper()
+saveWallpaper("Aoi ogata", ".jpg", "1")
 set_wallpaper_per_monitor()
